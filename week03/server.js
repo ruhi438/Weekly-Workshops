5efd158207a77fd49f4b68cc42c08dab9e0fabb3
Aarohi <aarohi.gediya@students.mq.edu.au>
2026-06-06 10:10:40 +1000

    Update server.js

--- week03/server.js
+++ week03/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express(); 
-app.use(express.static("public")); // Serve static files 
+app.use(express.static("public")); 
 app.get("/", (req, res) => {
-     res.set("Cache-Control", "public, max-age=30"); // Cache for 30 seconds
+     res.set("Cache-Control", "public, max-age=30"); 
      res.send("Hello, this response is cached!");
 });
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
