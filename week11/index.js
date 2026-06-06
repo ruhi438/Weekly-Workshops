b69226c93f4da8426f6691aefaeca039f9319f75
Aarohi <aarohi.gediya@students.mq.edu.au>
2026-06-06 10:28:03 +1000

    Create index.js

---
+++ week11/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const cookieParser = require('cookie-parser');
+
+const app = express();
+ 
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+const data = {
+    likes: [],
+};
+
+const COOKIE_NAME = 'counter';
+
+app.get('/count', (req, res) => {
+
+    let count = 1;
+    if (COOKIE_NAME in req.cookies) {
+        count = req.cookies[COOKIE_NAME];
+        count += 2; 
+    }
+    res.cookie(COOKIE_NAME, count);
+    res.json({count: count});
+});
+
+app.get('/likes', (req, res) => {
+    res.json(data.likes);
+});
+
+app.post('/likes', (req, res) => {
+    if( req.body && 'thing' in req.body) {
+        data.likes.push(req.body.thing);
+        res.json({status: 'success'});
+    }  else {
+        res.json({status: 'error', message: 'missing thing'});
+    }
+});
+
+const port = 3123;
+console.log(`Server running at http://localhost:${port}`);
+app.listen(port);
